a8cc924e2adfa360481a0071001321e1cce203ae
Arjun Sajeev <arjun@shopalyst.com>
2018-10-20 11:02:40 +0530

    Update clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const formatDate = date => {
+const formatDateTime = date => {
   // DD/MM/YY hh:mm:ss
-  let day, month, year, hour, minute, second
-  day = date.getDay()
+  let day, month, year, hours, minutes, seconds
+  day = date.getDate()
   month = date.getMonth()
+  year = date.getFullYear()
+  hours = date.getHours()
+  minutes = date.getMinutes()
+  seconds = date.getSeconds()
+  return (
+    [day, month + 1, year].join('/') + ' ' + [hours, minutes, seconds].join(':')
+  )
 }
 
 export default class Clock extends React.Component {
@@ -20,6 +27,6 @@ export default class Clock extends React.Component {
     setInterval(this.tick, 1000)
   }
   render() {
-    return <div>{this.state.time}</div>
+    return <div>{formatDateTime(this.state.time)}</div>
   }
 }
