11288fbbbca1575cb19d3f9df8ec68f51176b6c5
Sahil Satishkumar <sahilsatishkumar@gmail.com>
2018-10-21 13:05:43 +0530

    clean up suggestion

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -3,22 +3,17 @@ import './Clock.css'
 import { formatDateTime } from '../../utils/Time'
 
 export default class Clock extends React.PureComponent {
-  state = {
-    time: new Date()
-  }
-  tick = () => {
-    this.setState({
-      time: new Date()
-    })
-  }
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000)
-  }
-  render() {
-    return <div className="Clock">{formatDateTime(this.state.time)}</div>
-  }
+    state = { time: new Date() }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
+    tick = (time = new Date()) => this.setState({time})
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000)
+    }
+    
+    render = () => <div className="Clock">{formatDateTime(this.state.time)}</div>
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,60 +6,55 @@ import SelectField from './Field/SelectField'
 import TextAreaField from './Field/TextAreaField'
 import Clock from '../Clock/'
 
-const getFieldElement = field => {
-  const similarTypes = ['text', 'number', 'date', 'password']
-  const { type } = field
-  if (similarTypes.includes(type)) {
-    return <GenericInputField field={field} />
-  } else if (type === 'radio' || type === 'checkbox') {
-    return <RadioCheckboxField field={field} />
-  } else if (type === 'select' || type === 'multi-select') {
-    return <SelectField field={field} />
-  } else if (type === 'text-area') {
-    return <TextAreaField field={field} />
-  } else {
-    console.warn(`Invalid element type ${type}`)
-    return <div />
-  }
-}
+const fieldMap = (field) => ({
+    'text': <GenericInputField field={field} />,
+    'number': <GenericInputField field={field} />,
+    'date': <GenericInputField field={field} />,
+    'password': <GenericInputField field={field} />,
+    'radio': <RadioCheckboxField field={field} />,
+    'checkbox': <RadioCheckboxField field={field} />,
+    'select': <SelectField field={field} />,
+    'multi-select': <SelectField field={field} />,
+    'text-area': <TextAreaField field={field} />
+})
+
+const getFieldElement = (field) => fieldMap(field) ? fieldMap(field)[ field.type ] : null
 
 const renderField = (field, initialValues) => {
-  const {
-    name,
-    label,
-    classes: { groupClassName, labelClassName }
-  } = field
-  return (
-    <div className={groupClassName} key={name}>
-      <label className={labelClassName}>{label}</label>
-      {getFieldElement(field, initialValues)}
-    </div>
-  )
+    const {
+        name,
+        label,
+        classes: { groupClassName, labelClassName }
+    } = field
+    return (
+        <div className={groupClassName} key={name}>
+            <label className={labelClassName}>{label}</label>
+            {getFieldElement(field, initialValues)}
+        </div>
+    )
+}
+
+const handleFormSubmit = (e, onSubmit) => {
+    e.preventDefault()
+    const data = new FormData(e.target)
+    onSubmit && onSubmit(data)
 }
 
 export default function Form(props) {
-  const { formData } = props
-  const { title, onSubmit, initialValues, showTime, metaData } = formData
-  metaData.forEach(field => {
-    if (initialValues && initialValues[field.name]) {
-      field.initialValue = initialValues[field.name]
-    }
-  })
-  return (
-    <form
-      className="Form"
-      onSubmit={e => {
-        e.preventDefault()
-        const data = new FormData(e.target)
-        onSubmit && onSubmit(data)
-      }}
-    >
-      <h1>{title}</h1>
-      {showTime && <Clock />}
-      {metaData.map(renderField)}
-      <div className="submit-container">
-        <input type="submit" />
-      </div>
-    </form>
-  )
+    const { formData } = props
+    const { title, onSubmit, initialValues = {}, showTime, metaData } = formData
+    const formMetaData = metaData.map(field => initialValues.hasOwnProperty(field.name) ? ({ ...field, initialValue: initialValues[ field.name ] }) : field)
+    return (
+        <form
+            className="Form"
+            onSubmit={e => handleFormSubmit(e, onSubmit)}
+        >
+            <h1>{title}</h1>
+            {showTime ? <Clock /> : null}
+            {formMetaData.map(renderField)}
+            <div className="submit-container">
+                <input type="submit" />
+            </div>
+        </form>
+    )
 }
